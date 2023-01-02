c30e950a1ab23cd303b35357d91bbd92f01b40e4
Sara Nordström <sara@homebase.se>
2023-01-02 18:17:59 +0100

    Minor tests of Main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,47 +1,41 @@
-#include "Session.h"
-#include "Label.h"
 #include <SDL2/SDL.h>
-#include "Button.h"
+
 #include <string>
+
+#include "Button.h"
+#include "Label.h"
+#include "Session.h"
 using namespace std;
 using namespace cwing;
 
 int value = 0;
 
 class OkaKnapp : public Button {
-    public:
-        OkaKnapp(Label *lbl):Button(100, 100, 200, 70, "Öka"), label(lbl) {};
-        void perform(Button* source) {
-            value++;
-            label->setText(to_string(value));
-        }
-    private:
-        Label* label;
+   public:
+    OkaKnapp(Label* lbl) : Button(100, 100, 200, 70, "Öka"), label(lbl){};
+    void perform(Button* source) {
+        value++;
+        label->setText(to_string(value));
+    }
+
+   private:
+    Label* label;
 };
 
-//test
+// test
 
-int main(int argc, char** argv){
-
-/*
+int main(int argc, char** argv) {
     Session ses;
     Label* lbl = Label::getInstance(400, 100, 200, 70, "0");
+    // Button* b = Button::getInstance(100, 100, 200, 70, "Knapp");
+    // ses.add(b);
 
     ses.add(lbl);
 
-    Button* b = new OkaKnapp(lbl);
-    ses.add(b);
+    Button* b1 = new OkaKnapp(lbl);
+    ses.add(b1);
     lbl->setText("Hoppsan");
     ses.run();
 
     return 0;
-
-    */ 
-    Session ses;
-    Button* b = Button::getInstance(100, 100, 200, 70, "Hoppsan");
-    ses.add(b);
-    ses.run();
-    return 0;
-
-
 }
