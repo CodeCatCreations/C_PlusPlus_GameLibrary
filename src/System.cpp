168cadd5d11d0fa47ae40bd1b38ba8e5b3556556
vikkmaster <virraan@live.se>
2023-01-05 16:54:32 +0100

    testing to make it work

--- src/System.cpp
+++ src/System.cpp
@@ -3,12 +3,14 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
-namespace cwing {
+namespace cwing
+{
 
-	System::System() {
+	System::System()
+	{
 		SDL_Init(SDL_INIT_EVERYTHING);
 		// win = SDL_CreateWindow("Cwing", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, 0);
-		win = SDL_CreateWindow("GameDemo", 10, 10, 700, 500, 0);
+		win = SDL_CreateWindow("GameDemo", 20, 20, 700, 500, 0);
 		ren = SDL_CreateRenderer(win, -1, 0);
 		Mix_OpenAudio(20050, AUDIO_S16SYS, 2, 4096);
 		TTF_Init();
@@ -19,7 +21,8 @@ namespace cwing {
 		Mix_PlayChannel(-1, musik, -1);
 	}
 
-	System::~System() {
+	System::~System()
+	{
 		Mix_FreeChunk(musik);
 		Mix_CloseAudio();
 		TTF_CloseFont(font);
@@ -29,11 +32,13 @@ namespace cwing {
 		SDL_Quit();
 	}
 
-	SDL_Renderer* System::get_ren() const {
+	SDL_Renderer *System::get_ren() const
+	{
 		return ren;
 	}
 
-	TTF_Font* System::get_font() const {
+	TTF_Font *System::get_font() const
+	{
 		return font;
 	}
 
